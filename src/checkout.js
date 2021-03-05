8a4e498d94f8858d7cb01fae78adfb4068b302df
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-03-05 19:53:37 +0000

    added some basic functionality to calculate the prices and return prices

---
+++ src/checkout.js
@@ -0,0 +1,29 @@
+class Checkout {
+
+  constructor(name) {
+    this.priceList = {'CF1': 11.23, 'SR1': 5.00, 'FR1': 3.11};
+    this.order = []
+    this.totalprice = []
+  }
+
+  getPrice(item) {
+    var price = this.priceList[item];
+    this.totalprice.push(price)
+  }
+
+  addToCheckout(item) {
+    this.order.push(item);
+  }
+
+  allOrders() {
+    return this.order
+  }
+
+  finalPrice() {
+    var sum = this.totalprice.reduce(function(a, b){
+        return a + b;
+    }, 0);
+    return sum;
+  }
+
+}
---
+++ src/interface.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  var checkout = new Checkout();
+
+  reloadAllOrders();
+
+  $('#add-coffee').click(function() {
+    checkout.addOrder('CF1');
+    checkout.getPrice('CF1');
+    reloadAllOrders();
+    reloadTotalPrice();
+  })
+
+  $('#add-strawberries').click(function() {
+    checkout.addOrder('SR1');
+    checkout.getPrice('SR1');
+    reloadAllOrders();
+    reloadTotalPrice();
+  })
+
+  $('#add-tea').click(function() {
+    checkout.addOrder('FR1');
+    checkout.getPrice('FR1');
+    reloadAllOrders();
+    reloadTotalPrice();
+  })
+
+  function reloadAllOrders() {
+    $('#placefill').text(checkout.allOrders());
+  }
+
+  function reloadTotalPrice() {
+    $('#placefill2').text("£" + checkout.finalPrice());
+  }
+})
